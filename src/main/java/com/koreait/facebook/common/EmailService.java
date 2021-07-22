94aac2e8c2cde2c4fd018735f6bbfeb8b987bab3
HEEJU-a <81272648+HEEJU-a@users.noreply.github.com>
2021-07-22 09:45:09 +0900

    Update EmailService.java

--- src/main/java/com/koreait/facebook/common/EmailService.java
+++ src/main/java/com/koreait/facebook/common/EmailService.java
@@ -3,4 +3,5 @@ package com.koreait.facebook.common;
 public interface EmailService {
     void sendSimpleMessage(String to, String subject, String text);
     void sendMimeMessage(String to, String subject, String text);
+    
 }
